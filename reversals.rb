bb09c71cae11cb01c96411c2e0da2d989fd1268f
Carol Yao <c.y-@hotmail.com>
2016-06-09 09:42:00 -0400

    Added other solutions

--- reversals.rb
+++ reversals.rb
@@ -5,8 +5,21 @@ words = %w(first second third fourth fifth sixth)
 # so that the array is reversed and the individual words are reversed
 # ['htxis', 'htfif', 'htruof', 'driht', 'dnoces', 'tsrif']
 
+
+# this solves the map! problem since it doesn't produce a new array
+words.reverse!.each do |word|
+  word.reverse!
+end
+
+
+# this is not desired since: map produces a new array so it changes the original array as well as produce a new array
 words.reverse!.map do |word|
   word.reverse!
 end
 
-# used map instead of each to recreate an array 
+# used map instead of each to recreate an array
+
+# another way would be
+words.reverse.map! do |word|
+  word.reverse!
+end
