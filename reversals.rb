089e16364039c3fb7c241a00588620fbb4660f8b
Carol Yao <c.y-@hotmail.com>
2016-06-09 09:33:03 -0400

    Changed .each to .map to reproduce an array

--- reversals.rb
+++ reversals.rb
@@ -5,6 +5,8 @@ words = %w(first second third fourth fifth sixth)
 # so that the array is reversed and the individual words are reversed
 # ['htxis', 'htfif', 'htruof', 'driht', 'dnoces', 'tsrif']
 
-words.reverse!.each do |word|
+words.reverse!.map do |word|
   word.reverse!
 end
+
+# used map instead of each to recreate an array 
