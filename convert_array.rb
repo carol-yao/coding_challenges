53bf7db87a2758ae1d73f62cf780cfa97af79b38
Carol Yao <c.y-@hotmail.com>
2016-06-14 10:06:24 -0400

    Refactored and added another method

--- convert_array.rb
+++ convert_array.rb
@@ -3,7 +3,7 @@
 # Pseudo Code
 # Iterate through each item in the array (which are arrays)
 # Shovel the first item into an empty array how many times the second item says
-  # keep track using a counter that increments each time 
+  # keep track using a counter that increments each time
 
 def convert(input)
   array = []
@@ -15,4 +15,26 @@ def convert(input)
     end
   end
   array
+end
+
+# ====
+# Refactoring
+# this gets rid of the counter and repeats it n.times
+
+def convert(input)
+  array = []
+
+  input.each do |pair|
+  pair[1].times do
+      array << pair[0]
+    end
+  end
+
+  array
+end
+# ====
+
+# it maps, and then flattens since passing in two values
+def row(input)
+  input.flat_map do |word, number| [word] * number end
 end
